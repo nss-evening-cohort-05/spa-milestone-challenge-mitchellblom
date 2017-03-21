8008394e86f203f54fb0e7d143f1a0300a017d92
Mitchell Blom <mitchellblom@gmail.com>
2017-03-21 13:12:05 -0500

    logging car objects from json

--- js/CarLot.js
+++ js/CarLot.js
@@ -1,15 +1,28 @@
-var CarLot = (function () {
+var CarLot = (function (cArray) {
 
   var inventory = [];
+  var inventoryLoader = new XMLHttpRequest();
 
-  return {
-    loadInventory: function (callback) {
-      var inventoryLoader = new XMLHttpRequest();
 
-      inventoryLoader.addEventListener("load", function () {
+    loadInventory = function (callback) {	// data becomes callback
+      	console.log(callback);
+    };
 
-      });
-    }
-  };
+function executeOnLoad() {
+    var data = JSON.parse(this.responseText);
+    console.log("getting to execute...");
+    loadInventory(data);
+}
 
-})();
+function executeOnFail() {
+    alert("Page failed to load.");
+}
+
+inventoryLoader.addEventListener("load", executeOnLoad);
+inventoryLoader.addEventListener("error", executeOnFail);
+inventoryLoader.open("GET", "inventory.json");
+inventoryLoader.send();
+
+	return cArray;
+
+})(CarLot || {});
