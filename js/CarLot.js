23be47c81451d64db178ec2b2ea2fd2dcb69501c
Mitchell Blom <mitchellblom@gmail.com>
2017-03-21 13:26:05 -0500

    json object into private array

--- js/CarLot.js
+++ js/CarLot.js
@@ -1,28 +1,28 @@
-var CarLot = (function (cArray) {
+var CarLot = (function(cArray) {
 
-  var inventory = [];
-  var inventoryLoader = new XMLHttpRequest();
+    var inventory = [];
+    var inventoryLoader = new XMLHttpRequest();
 
-
-    loadInventory = function (callback) {	// data becomes callback
-      	console.log(callback);
+    loadInventory = function(callback) { // data becomes callback
+        console.log(callback.cars.length);
+        for (var i = 0; i < callback.cars.length; i++) {
+        	inventory.push(callback.cars[i]);
+        }
+        	console.log("inventory to pass to public: ", inventory);
     };
 
-function executeOnLoad() {
-    var data = JSON.parse(this.responseText);
-    console.log("getting to execute...");
-    loadInventory(data);
-}
+    function executeOnLoad() {
+        var data = JSON.parse(this.responseText);
+        loadInventory(data);
+    };
 
-function executeOnFail() {
-    alert("Page failed to load.");
-}
+    function executeOnFail() {
+        alert("Page failed to load.");
+    };
 
-inventoryLoader.addEventListener("load", executeOnLoad);
-inventoryLoader.addEventListener("error", executeOnFail);
-inventoryLoader.open("GET", "inventory.json");
-inventoryLoader.send();
-
-	return cArray;
+    inventoryLoader.addEventListener("load", executeOnLoad);
+    inventoryLoader.addEventListener("error", executeOnFail);
+    inventoryLoader.open("GET", "inventory.json");
+    inventoryLoader.send();
 
 })(CarLot || {});
--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
-// function populatePage (inventory) {
-//   // Loop over the inventory and populate the page
+function populatePage (inventory) {
+  CarLot.loadInventory();
   
-//   // Now that the DOM is loaded, establish all the event listeners needed
-//   CarLot.activateEvents();
-// }
+  // Now that the DOM is loaded, establish all the event listeners needed
 
-// // Load the inventory and send a callback function to be
-// // invoked after the process is complete
+}
+
+
+
+
+
+
+
+// Load the inventory and send a callback function to be
+// invoked after the process is complete
 // CarLot.loadInventory();
