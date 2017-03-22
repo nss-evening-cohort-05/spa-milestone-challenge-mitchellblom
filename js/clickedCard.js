92450630664f3fb8d3c6e436821f98e3231366a4
Mitchell Blom <mitchellblom@gmail.com>
2017-03-21 20:40:39 -0500

    parent and child div clicks recolor border

--- js/clickedCard.js
+++ js/clickedCard.js
@@ -3,21 +3,24 @@ var CarLot = (function(cArray) {
     var carCard = document.getElementsByClassName("car-card");
 
     window.addEventListener("click", function(e) {
-        for (var k = 0; k < cArray.getInventory().length; k++) {
-            console.log(carCard[k].classList);
-            carCard[k].classList.remove("clicked");
+        for (var i = 0; i < cArray.getInventory().length; i++) {
+            // console.log(carCard[i].classList);
+            carCard[i].classList.remove("clicked");
         }
-        if (e.target.classList.value === "car-card") {
+        if (e.target.classList.value === "car-card" ) {
             for (var j = 0; j < cArray.getInventory().length; j++) {
-                // console.log(e);
                 e.target.classList.add("clicked");
-                // console.log(carCard[j].classList);
             }
+        }
+            else if (e.target.parentNode.classList.value === "car-card") {
+                console.log("table click firing parentNode: ", e.target.parentNode);
+                e.target.parentNode.classList.add("clicked");
+
             //     console.log("e.target.classList", e.target.classList);
             // //     e.target.classList.add("clicked");
             // // } else if (e.target.parentNode.parentNode.classList.value === "carCard") {
             // //     e.target.parentNode.parentNode.classList.add("clicked");
-        }
+    }
     });
 
     return cArray;
